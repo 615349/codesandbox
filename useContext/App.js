b2276eac29a15bc74e368165661af6198f428ec0
Ben Li <ben.li@optus.com.au>
2019-11-13 15:54:41 +1100

    useContext

---
+++ useContext/App.js
@@ -0,0 +1,50 @@
+import React, { useContext, useState } from "react";
+import "./styles.css";
+import { StoreContext } from "./context/StoreContext";
+import { types } from "./context/reducers";
+
+function App() {
+  const { state, dispatch } = useContext(StoreContext);
+  const [techInput, setTechInput] = useState("");
+
+  return (
+    <div className="App">
+      <h1>Hooks - The Redux Killer</h1>
+      <p>
+        Try to add duplicate items with both Direct dispatch and Action logic
+      </p>
+      <div className="form">
+        <input
+          name="tech"
+          value={techInput}
+          onChange={e => setTechInput(e.target.value)}
+        />
+        <button
+          type="button"
+          onClick={() =>
+            dispatch({ type: types.ADD_TO_TECH_LIST, payload: techInput })
+        }
+        >
+          dispatch
+        </button>
+      </div>
+
+      <h3>state.techList</h3>
+      <ul>
+      {state.techList.map(tech => (
+        <li key={tech}>
+          {tech}
+          <button
+          onClick={() =>
+            dispatch({ type: types.REMOVE_FROM_TECH_LIST, payload: tech })
+          }>
+            Remove
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+}
+
+export default App;
